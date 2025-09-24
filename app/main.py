5a9e0e2f7822d88eb2c982eff45278ea753eada8
kushalraga <kushalraga20@gmail.com>
2025-09-24 17:31:46 +0530

    requirements update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"message": "service up and running"}
+
+key = os.getenv("key")
+endpoint = os.getenv("endpoint")
